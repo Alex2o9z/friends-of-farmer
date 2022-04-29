1db4ae283a1e9b23593e6362a6257440ca697b41
Alex2o9z <hothangtrinh200902@gmail.com>
2022-04-29 22:35:26 +0700

    add project files

---
+++ KhuyenNong/function.js
@@ -0,0 +1,33 @@
+// Header
+function responsiveMenu() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.classList.add("responsive");
+	} else if (x.className === "topnav sticky") {
+		x.classList.add("responsive");
+	} else {
+		x.classList.remove("responsive");
+	}
+}
+
+// Get the modal
+var modal = document.getElementById('signIn');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == signIn) {
+        modal.style.display = "none";
+    }
+}
+
+<!-- Footer -->
+
+function copyToClipboard(elementId) {
+	var aux = document.createElement("input");
+	aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+	document.body.appendChild(aux);
+	aux.select();
+	document.execCommand("copy");
+	document.body.removeChild(aux);
+	alert("Đã sao chép vào khay nhớ tạm:\n" + aux.value);
+}
---
+++ TrangChu/function.js
@@ -0,0 +1,25 @@
+
+<!-- Header -->
+function responsiveMenu() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.classList.add("responsive");
+	} else if (x.className === "topnav sticky") {
+		x.classList.add("responsive");
+	} else {
+		x.classList.remove("responsive");
+	}
+}
+
+
+<!-- Footer -->
+
+function copyToClipboard(elementId) {
+	var aux = document.createElement("input");
+	aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+	document.body.appendChild(aux);
+	aux.select();
+	document.execCommand("copy");
+	document.body.removeChild(aux);
+	alert("Đã sao chép vào khay nhớ tạm:\n" + aux.value);
+}
